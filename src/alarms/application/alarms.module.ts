ac7068241ad83f76ba641af2d64c9b1321ed1a37
Emil Juchnikowski <emil@juchnikowski.com>
2023-12-27 11:52:09 +0100

    Sagas

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -7,6 +7,9 @@ import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
 import { AlarmCreatedEventHandler } from './event-handlers/alarm-created.event-handler';
 import { AcknowledgeAlarmCommandHandler } from './commands/acknowledge-alarm.command-handler';
 import { AlarmAcknowledgedEventHandler } from './event-handlers/alarm-acknowledged.event-handler';
+import { CascadingAlarmsSaga } from './sagas/cascading-alarms.saga';
+import { NotifyFacilitySupervisorCommandHandler } from './commands/notify-facility-supervisor.command-handler';
+import { UnacknowledgedAlarmsSaga } from './sagas/unacknowledged-alarms.saga';
 
 @Module({
   controllers: [AlarmsController],
@@ -18,11 +21,13 @@ import { AlarmAcknowledgedEventHandler } from './event-handlers/alarm-acknowledg
     AlarmCreatedEventHandler,
     AcknowledgeAlarmCommandHandler,
     AlarmAcknowledgedEventHandler,
+    CascadingAlarmsSaga,
+    NotifyFacilitySupervisorCommandHandler,
+    UnacknowledgedAlarmsSaga,
   ],
 })
 export class AlarmsModule {
   static withInfrastucture(infrastructureModule: Type | DynamicModule) {
-    // 👈 new static method
     return {
       module: AlarmsModule,
       imports: [infrastructureModule],
